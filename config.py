cda808fe0ddbe30d1b246c5f203c203c8a354d82
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-19 12:34:06 -0800

    Update config.py.

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002108419450")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "ultroid_official")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002405500273"))
 
 
